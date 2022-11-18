03e899c76498b367f7d75e20fffa075be7376232
Allison Grothman <allison.grothman@ucalgary.ca>
2022-11-17 23:34:45 -0700

    Create and read a test input file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #main.py
 import schedule
+import sys
+
+def parse(file):
+    try:
+        inputFile = open(file, 'r')
+    except:
+        sys.exit("The first command line argument must be a text file.")
+    
+    fileContent = inputFile.readlines()
+    print(fileContent)
+    
+    inputFile.close()
 
 #creates an empty schedule
 p1 = schedule.Schedule(5) #Note: 5 is a temp value, replace after writing parser
@@ -16,4 +28,10 @@ p1.addPractice(4,4,'P4')
 #prints the schedule
 p1.print()
 
+#read the input text file from the command line
+try:
+    file = sys.argv[1]
+except:
+    sys.exit("Must provide a program description in a text file.")
 
+parse(file)
