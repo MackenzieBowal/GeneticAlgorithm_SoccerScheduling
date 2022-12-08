bc0c31e42aee5fe071564174a98a974014234cbe
RachelRalph <60715884+RachelRalph@users.noreply.github.com>
2022-12-08 13:08:06 -0700

    Revert "Fix bug with partial assignments"
    This reverts commit bbf5e6b93d054dd6d88fb24f445c2e0d0f7ba34f.

--- constrFunction.py
+++ constrFunction.py
@@ -16,7 +16,8 @@ def initiateConstr(gl, pl, vgs, vps, nc, unw, pref, pr, pa):
     pair = pr
     partAssign = pa
 
-def oneBigConstr(sch):
+def oneBigConstr(sch, isGame, time, day):
+    """
     for i in range(len(days)):
         for j in range(len(times)):
             slot = sch.getSchedule()[i][j]
@@ -89,11 +90,87 @@ def oneBigConstr(sch):
                         if ("CMSA U13T1 " in game):
                             return False
                     break
+
+
+                """
+
+    #if(isGame):
+    slot = sch.getSchedule()[day][time]
+    slotGames = slot.getGames()
+    slotPracs = slot.getPractices()
+
+    # hc4
+    for game in slotGames:
+        gameComponents = game.split(" ")
+        for prac in slotPracs:
+            pracComponents = prac.split(" ")
+            if (gameComponents[0] == pracComponents[0] and gameComponents[1] == pracComponents[1] and \
+                    (len(pracComponents) < 6 or gameComponents[3] == pracComponents[3])):
+                return False
+
+    # hc5
+    for (a, b) in notCompatible:
+        if((a in slotGames or a in slotPracs) and (b in slotGames or b in slotPracs)):
+                return False
+
+    # hc6
+    if j < 20:
+        for game in slotGames:
+            if ("DIV 9" in game):
+                return False
+        for prac in slotPracs:
+            if ("DIV 9" in prac):
+                return False
+    
+    # hc7
+    for unwant_sched in unwanted:
+        gameorprac = unwant_sched[0]
+        day = unwant_sched[1]
+        time = unwant_sched[2]
+        for game in sch.getSchedule()[days[day]][times[time]].getGames():
+            if(game == gameorprac):
+                return False
+        for prac in sch.getSchedule()[days[day]][times[time]].getPractices():
+            if(prac == gameorprac):
+                return False
+
+    # hc8
+    sched = sch.getSchedule()
+    if(len(sched[days["TU"]][6].getGames()) != 0):
+        return False
+
+    # hc9
+    for game1 in slotGames:
+        if ("U15" in game1 or "U16" in game1 or "U17" in game1 or "U19" in game1):
+            for game2 in slotGames:
+                if ("U15" in game2 or "U16" in game2 or "U17" in game2 or "U19" in game2) and (game1 != game2):
+                    return False
+
+    # hc10
+    for p in slotPracs:
+        # check U12T1S
+        if ("CMSA U12T1S" in p):
+            for prac in slotPracs:
+                if ("CMSA U12T1 " in prac):
+                    return False
+            for game in slotGames:
+                if ("CMSA U12T1 " in game):
+                    return False
+        # check U13T1S
+        if ("CMSA U13T1S" in p):
+            for prac in slotPracs:
+                if ("CMSA U13T1 " in prac):
+                    return False
+            for game in slotGames:
+                if ("CMSA U13T1 " in game):
+                    return False
+            break
+
     return True
 
 
 # THIS IS A SUPER BASIC IMPLEMENTATION OF CONSTR() TO TEST REPAIR TREE - THIS STILL NEEDS TO BE PROPERLY IMPLEMENTED BASED ON pg. 2 OF REPORT 
-def constr(sch):
+def constr(sch, time, day):
 
     #hc1 = check_hc1(sch)
     #hc2 = check_hc2(sch)
@@ -109,4 +186,4 @@ def constr(sch):
 
     return hc4 and hc5 and hc6 and hc7 and hc8 and hc9 and hc10
     '''
-    return oneBigConstr(sch)
+    return oneBigConstr(sch, time, day)
